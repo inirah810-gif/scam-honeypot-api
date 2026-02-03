5ba6d832ba08de5ee31095e17cafe22ba8dd95db
Harini <inirah810@gmail.com>
2026-02-03 07:31:44 +0530

    simplify endpoint

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from fastapi import FastAPI, Header, Body
+from fastapi import FastAPI, Header
 from typing import Optional
 
 app = FastAPI()
 
 @app.post("/interact")
-async def interact(
-    x_api_key: Optional[str] = Header(None),
-    data: Optional[dict] = Body(None)
-):
-    return {
-        "status": "Honeypot is live",
-        "received": data
-    }
+async def interact(x_api_key: Optional[str] = Header(None)):
+    return {"status": "Honeypot is live"}
 
        
